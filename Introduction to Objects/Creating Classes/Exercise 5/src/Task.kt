00d58fa9e0005d82b8d411342535ce23ab7a9b14
pevgen <pevgen@gmail.com>
2024-05-15 11:52:12 +0300

    "Creating Classes" is done

--- Introduction to Objects/Creating Classes/Exercise 5/src/Task.kt
+++ Introduction to Objects/Creating Classes/Exercise 5/src/Task.kt
@@ -1,11 +1,18 @@
 // CreatingClasses/Task5.kt
 package creatingClassesExercise5
 
-fun cap(s: String) = s  
+import java.util.*
+
+fun cap(s: String) = s.replaceFirstChar {
+    if (it.isLowerCase())
+        it.titlecase(Locale.getDefault())
+    else
+        it.toString()
+}
 
 fun main() {
-  println(cap("hi!"))
-  println(cap("Hi!"))
+    println(cap("hi!"))
+    println(cap("Hi!"))
 }
 /* Output:
 Hi!

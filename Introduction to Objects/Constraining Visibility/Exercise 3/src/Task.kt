1fd1c9c5018fdc6675959454aee67731325dbfca
pevgen <pevgen@gmail.com>
2024-05-18 11:18:41 +0300

    "Constraining Visibility" is done

--- Introduction to Objects/Constraining Visibility/Exercise 3/src/Task.kt
+++ Introduction to Objects/Constraining Visibility/Exercise 3/src/Task.kt
@@ -2,49 +2,65 @@
 package constrainingVisibilityExercise3
 
 class Robot(
-  private val fieldSize: Int,
-  private var x: Int,
-  private var y: Int
+    private val fieldSize: Int,
+    private var x: Int,
+    private var y: Int
 ) {
-  private fun crossBoundary(coordinate: Int): Int {
-    val inBounds = coordinate % fieldSize
-    return if (inBounds < 0) {
-      fieldSize + inBounds
-    } else {
-      inBounds
+    private fun crossBoundary(coordinate: Int): Int {
+        val inBounds = coordinate % fieldSize
+        return if (inBounds < 0) {
+            fieldSize + inBounds
+        } else {
+            inBounds
+        }
     }
-  }
 
-  
+    private fun checkPositive(steps: Int): Boolean {
+        return if (steps <= 0) {
+            println("steps argument must be positive, is $steps")
+            false
+        } else {
+            true
+        }
 
-  fun right(steps: Int) {
-    x += steps
-  x = crossBoundary(x)
-  }
+    }
 
-  fun left(steps: Int) {
-    x -= steps
-  x = crossBoundary(x)
-  }
+    fun right(steps: Int) {
+        if (checkPositive(steps)) {
+            x += steps
+            x = crossBoundary(x)
+        }
+    }
 
-  fun down(steps: Int) {
-    y += steps
-  y = crossBoundary(y)
-  }
+    fun left(steps: Int) {
+        if (checkPositive(steps)) {
+            x -= steps
+            x = crossBoundary(x)
+        }
+    }
 
-  fun up(steps: Int) {
-    y -= steps
-  y = crossBoundary(y)
-  }
+    fun down(steps: Int) {
+        if (checkPositive(steps)) {
+            y += steps
+            y = crossBoundary(y)
+        }
+    }
 
-  fun getLocation(): String = "($x, $y)"
+    fun up(steps: Int) {
+        if (checkPositive(steps)) {
+            y -= steps
+            y = crossBoundary(y)
+        }
+    }
 
-  override fun toString() = "Robot(x=$x,y = $y)"
+    fun getLocation(): String = "($x, $y)"
+
+    override fun toString() = "Robot(x=$x,y = $y)"
 }
 
 fun main() {
-  val robot = Robot(10, 1, 1)
-  robot.right(-1)
+    val robot = Robot(10, 1, 1)
+    robot.right(-1)
 }
 /* Output:
 steps argument must be positive, is -1

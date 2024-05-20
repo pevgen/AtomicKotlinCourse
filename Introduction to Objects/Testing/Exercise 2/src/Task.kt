2b5ce969c75a2990c8d228c0205ac019ab0ae27b
pevgen <pevgen@gmail.com>
2024-05-20 20:45:09 +0300

    "Testing" is done

--- Introduction to Objects/Testing/Exercise 2/src/Task.kt
+++ Introduction to Objects/Testing/Exercise 2/src/Task.kt
@@ -3,8 +3,12 @@ package testingExercise2
 import atomictest.eq
 import atomictest.neq
 
-fun sum(a: Int, b: Int, c: Int): Int = TODO()
+fun sum(a: Int, b: Int, c: Int): Int = a + b+ c
 
 fun main() {
-  TODO()
+  sum(0,0,0) eq 0
+  sum(1,0,0) neq 0
+  sum(1,-1,2) eq 2
+  sum(0,1,-5) neq -5
+  sum(100,-99,-2) eq -1
 }
--- Introduction to Objects/Testing/Exercise 3/src/Task.kt
+++ Introduction to Objects/Testing/Exercise 3/src/Task.kt
@@ -4,13 +4,20 @@ import atomictest.eq
 
 fun squareArea(x: Int) = x * x
 
-fun rectangleArea(x: Int, y: Int) = x * x
+fun rectangleArea(x: Int, y: Int) = x * y
 
-fun trapezoidArea(x: Int, y: Int, h: Int) = h / 2 * (x + y)
+fun trapezoidArea(x: Int, y: Int, h: Int) = h / 2.0 * (x + y)
 
 fun main() {
   squareArea(1) eq 1
   squareArea(2) eq 4
   squareArea(5) eq 25
-  // add more tests
+
+  rectangleArea(0,2) eq 0
+  rectangleArea(100,0) eq 0
+  rectangleArea(5,6) eq 30
+
+  trapezoidArea(3,4, 1) eq 3.5
+  trapezoidArea(1,1, 4) eq 4.0
+  trapezoidArea(100,200, 1200) eq 180000.0
 }
